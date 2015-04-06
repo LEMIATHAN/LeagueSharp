05f496a5da7456031930f453023b544cae8bf3aa
LEMIATHAN <LEMIATHAN@users.noreply.github.com>
2015-04-06 09:04:38 +0100

    Update Program.cs

--- MysticalBard/MysticalBard/Program.cs
+++ MysticalBard/MysticalBard/Program.cs
@@ -17,12 +17,14 @@ namespace MysticalBard
         public static Menu Menu;
         private static Obj_AI_Hero Player;
 
+
         public static Orbwalking.Orbwalker Orbwalker;
 
         public static Spell Q;
         public static Spell R;
 
         public static Spell stunQ { get; private set; }
+
 
         public static void Main(string[] args)
         {
@@ -32,61 +34,69 @@ namespace MysticalBard
                 Game_Start(new EventArgs());
             }
         }
-        
+
         public static void Game_Start(EventArgs args)
         {
             Player = ObjectManager.Player;
 
             if (Player.ChampionName != "Bard")
                 return;
-
+            
             Menu = new Menu("Mystical Bard", "Mystical Bard", true);
             var TargetSelectorMenu = new Menu("Target Selector", "Target Selector");
             TargetSelector.AddToMenu(TargetSelectorMenu);
             Menu.AddSubMenu(TargetSelectorMenu);
 
+
             Menu.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
             Orbwalker = new Orbwalking.Orbwalker(Menu.SubMenu("Orbwalking"));
 
+
+            //------------Combo
             Menu.AddSubMenu(new Menu("Combo", "Combo"));
             Menu.SubMenu("Combo").AddItem(new MenuItem("UseQ", "Use Q").SetValue(true));
             Menu.SubMenu("Combo").AddItem(new MenuItem("UseR", "Use R").SetValue(true));
-            Menu.SubMenu("Combo").AddItem(new MenuItem("Stun", "Q stun restriction")).SetValue(true);
-            Menu.SubMenu("Combo").AddItem(new MenuItem("MinEnemies", "Minimum enemies for ult")).SetValue(new Slider(3, 5, 1));
-            Menu.SubMenu("Combo").AddItem(new MenuItem("cActive", "Combo").SetValue(new KeyBind(32, KeyBindType.Press)));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("aQStun", "Q Stun Restriction!")).SetValue(true);
+            Menu.SubMenu("Combo").AddItem(new MenuItem("MinEnemies", "Minimum Enemy Count for Ultimate")).SetValue(new Slider(3, 5, 1));
+            Menu.SubMenu("Combo").AddItem(new MenuItem("ComboActive", "Combo!").SetValue(new KeyBind(32, KeyBindType.Press)));
+            //-------------end Combo
+
 
             Menu.AddSubMenu(new Menu("Harass", "Harass"));
             Menu.SubMenu("Harass").AddItem(new MenuItem("HarassQ", "Use Q").SetValue(false));
-            Menu.SubMenu("Harass").AddItem(new MenuItem("hActive", "Harass").SetValue(new KeyBind("20".ToCharArray()[0], KeyBindType.Press)));
+            Menu.SubMenu("Harass").AddItem(new MenuItem("HarassActive", "Harass!").SetValue(new KeyBind("20".ToCharArray()[0], KeyBindType.Press)));
+
 
             Menu.AddSubMenu(new Menu("Clear", "Clear"));
-            Menu.SubMenu("Clear").AddItem(new MenuItem("FarmQ", "Use Q").SetValue(false));
-            Menu.SubMenu("Clear").AddItem(new MenuItem("wActive", "Clear").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
+            Menu.SubMenu("Clear").AddItem(new MenuItem("UseQFarm", "Use Q").SetValue(false));
+            Menu.SubMenu("Clear").AddItem(new MenuItem("ClearActive", "Clear!").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
 
-            // Taken from DesomondBard. Credit goes to Desomond, I had no idea what I was doing here :P vvvvvvvv
+
             var mana = Menu.AddSubMenu(new Menu("Misc", "Misc"));
-            mana.AddItem(new MenuItem("comboMana", "Combo Mana").SetValue(new Slider(1, 100, 0)));
-            mana.AddItem(new MenuItem("harassMana", "Harass Mana").SetValue(new Slider(30, 100, 0)));
-            mana.AddItem(new MenuItem("forceQ", "Force Q(slow not guarenteed").SetValue(new KeyBind("A".ToCharArray()[0], KeyBindType.Press)));
+            mana.AddItem(new MenuItem("cMana", "Combo Mana %").SetValue(new Slider(1, 100, 0)));
+            mana.AddItem(new MenuItem("hMana", "Harass Mana %").SetValue(new Slider(30, 100, 0)));
+            mana.AddItem(new MenuItem("forceQ", "Force Q").SetValue(new KeyBind("A".ToCharArray()[0], KeyBindType.Press)));
             mana.AddItem(new MenuItem("forceR", "Force R").SetValue(new KeyBind("T".ToCharArray()[0], KeyBindType.Press)));
-            mana.AddItem(new MenuItem("interruptR", "interrupt dangerous spells with Ult").SetValue(true));
+            mana.AddItem(new MenuItem("interruptR", "Interrupt Dangerous Spells with Ultimate").SetValue(true));
 
             Menu.AddSubMenu(new Menu("Draw", "Draw"));
-            Menu.SubMenu("Draw").AddItem(new MenuItem("DrawQ", "Draw Q").SetValue(new Circle(true, System.Drawing.Color.Green)));
-            Menu.SubMenu("Draw").AddItem(new MenuItem("DrawQ", "Draw Q").SetValue(new Circle(true, System.Drawing.Color.Green)));
+            Menu.SubMenu("Draw").AddItem(new MenuItem("DrawQ", "Draw Q").SetValue(new Circle(true, Color.Green)));
+            Menu.SubMenu("Draw").AddItem(new MenuItem("DrawR", "Draw R").SetValue(new Circle(true, Color.Green)));
+
 
             Menu.AddToMainMenu();
+
 
             Q = new Spell(SpellSlot.Q, 950f);
             R = new Spell(SpellSlot.R, 2500f);
             stunQ = new Spell(SpellSlot.Q, Q.Range);
-
+           
             Q.SetSkillshot(0.25f, 60, 1600, false, SkillshotType.SkillshotLine);
             R.SetSkillshot(0.5f, 325, 2100, false, SkillshotType.SkillshotCircle);
             stunQ.SetSkillshot(Q.Delay, Q.Width, Q.Speed, true, SkillshotType.SkillshotLine);
 
 
-            Game.PrintChat("MysticalBard Loaded.");
+            Game.PrintChat("DesomondBard Loaded.");
             Game.OnUpdate += Game_OnUpdate;
             Drawing.OnDraw += OnDraw;
             Interrupter2.OnInterruptableTarget += BardOnInterruptableSpell;
@@ -95,27 +105,27 @@ namespace MysticalBard
 
         public static void Game_OnUpdate(EventArgs args)
         {
-
+          
             Obj_AI_Hero t = null;
-            var wActive = Menu.Item("wActive").GetValue<KeyBind>().Active;
-            var hActive = Menu.Item("hActive").GetValue<KeyBind>().Active;
-            var cActive = Menu.Item("cActive").GetValue<KeyBind>().Active;
-            var harassMana = Menu.Item("harassMana").GetValue<Slider>().Value;
-            var comboMana = Menu.Item("comboMana").GetValue<Slider>().Value;
+            var ClearActive = Menu.Item("ClearActive").GetValue<KeyBind>().Active;
+            var HarassActive = Menu.Item("HarassActive").GetValue<KeyBind>().Active;
+            var ComboActive = Menu.Item("ComboActive").GetValue<KeyBind>().Active;
+            var hMana = Menu.Item("harassMana").GetValue<Slider>().Value;
+            var cMana = Menu.Item("comboMana").GetValue<Slider>().Value;
 
             var forceQ = Menu.Item("forceQ").GetValue<KeyBind>().Active;
-
+           
             var forceR = Menu.Item("forceR").GetValue<KeyBind>().Active;
-
-            if (wActive)
+           
+            if (ClearActive)
             {
                 Farm();
             }
-            if (hActive && harassMana < ((ObjectManager.Player.Mana / ObjectManager.Player.MaxMana) * 100))
+            if (HarassActive && hMana < ((ObjectManager.Player.Mana / ObjectManager.Player.MaxMana)*100))
             {
                 Harass(t);
             }
-            if (cActive && comboMana < ((ObjectManager.Player.Mana / ObjectManager.Player.MaxMana) * 100))
+            if (ComboActive && cMana < ((ObjectManager.Player.Mana / ObjectManager.Player.MaxMana)*100))
             {
                 Combo(t);
             }
@@ -142,7 +152,7 @@ namespace MysticalBard
         public static void Farm()
         {
             List<Vector2> pos = new List<Vector2>();
-            bool qFarm = Menu.Item("FarmQ").GetValue<bool>();
+            bool qFarm = Menu.Item("UseQFarm").GetValue<bool>();
 
             var AllMinions = MinionManager.GetMinions(Player.ServerPosition, Q.Range, MinionTypes.All, MinionTeam.Enemy, MinionOrderTypes.Health);
             foreach (var minion in AllMinions)
@@ -157,14 +167,14 @@ namespace MysticalBard
         public static void Harass(Obj_AI_Hero t)
         {
             var HarassQ = Menu.Item("HarassQ").GetValue<bool>();
-            var alwaysStun = Menu.Item("alwaysStun").GetValue<bool>();
+            var aQStun = Menu.Item("alwaysStun").GetValue<bool>();
             t = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
 
             if (HarassQ && Q.IsReady())
             {
                 if (t.IsValidTarget())
                 {
-                    if (alwaysStun)
+                    if (aQStun)
                     {
                         castStunQ(t);
                     }
@@ -178,18 +188,18 @@ namespace MysticalBard
 
         public static void Combo(Obj_AI_Hero t)
         {
-
-            var useQ = Menu.Item("UseQ").GetValue<bool>();
-            var useR = Menu.Item("UseR").GetValue<bool>();
-            var alwaysStun = Menu.Item("alwaysStun").GetValue<bool>();
-            var numOfEnemies = Menu.Item("MinEnemies").GetValue<Slider>().Value;
+           
+            var useQ = Menu.Item("UseQ").GetValue<bool>();    
+            var useR = Menu.Item("UseR").GetValue<bool>();       
+            var aQStun = Menu.Item("alwaysStun").GetValue<bool>();
+            var numOfEnemies = Menu.Item("MinEnemies").GetValue<Slider>().Value;    
             t = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
 
             if (useQ && Q.IsReady())
-            {
+            {  
                 if (t.IsValidTarget())
                 {
-                    if (alwaysStun)
+                    if (aQStun)
                     {
                         castStunQ(t);
                     }
@@ -226,10 +236,10 @@ namespace MysticalBard
 
         private static void BardOnInterruptableSpell(Obj_AI_Hero unit, Interrupter2.InterruptableTargetEventArgs args)
         {
-            if (Menu.Item("interruptR").GetValue<bool>())
-            {
-                R.Cast(unit, false, true);
-            }
+             if (Menu.Item("interruptR").GetValue<bool>())
+             {
+                R.Cast(unit,false, true);
+             }
         }
 
         private static void OnDraw(EventArgs args)
@@ -246,7 +256,7 @@ namespace MysticalBard
         }
 
         private static void castStunQ(Obj_AI_Hero target)
-        {
+        {   
             var prediction = stunQ.GetPrediction(target);
 
             var direction = (Player.ServerPosition - prediction.UnitPosition).Normalized();
